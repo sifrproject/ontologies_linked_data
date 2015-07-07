362fbc3c5c77472be531a0bf57f4606defdff2ca
Vincent Emonet <vincent.emonet@gmail.com>
2015-07-07 19:21:17 +0200

    fix external_class.rb

--- lib/ontologies_linked_data/models/external_class.rb
+++ lib/ontologies_linked_data/models/external_class.rb
@@ -1,25 +1,19 @@
 
 module LinkedData
   module Models
-    class ExternalClass < LinkedData::Models::Base
-      attribute :id, enforce: [:existence]
-      attribute :source, enforce: [:existence]
-      attribute :ontology
+    class ExternalClass
+
+      attr_reader :id, :source, :ontology
+
+      #attribute :id, enforce: [:existence]
+      #attribute :source, enforce: [:existence]
+      #attribute :ontology
 
       serialize_default :id, :source
 
       def initialize(id, source)
         @id = id
         @source = source
-      end
-      def id
-        return @id
-      end
-      def source
-        return @source
-      end
-      def ontology
-        return @ontology
       end
     end
   end
